0545972147fe66f5cc4e4a0d301afd0217700ec7
Emi <emi.bungo@gmail.com>
2019-11-05 13:20:12 -0500

    Created Boardgame and gameDetail domain objects and Mongoose models

---
+++ create-your-own-app-api/models/Boardgame.domain.js
@@ -0,0 +1,9 @@
+class Boardgame {
+  constructor(title, yearPublished, image) {
+    this.title = title;
+    this.yearPublished = yearPublished;
+    this.image = image;
+  }
+}
+
+module.exports = Boardgame;
---
+++ create-your-own-app-api/models/gameDetail.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({ categoryName: String });
+
+const gameDetailSchema = new Schema({
+    title: { ref: "Boardgame", type: mongoose.Schema.Types.ObjectId }
+    numberOfPlayers: { required: true, type: String } ,
+    ageRange: { required: true, type: String },
+    length: { required: true, type: String },
+    publisher: { required: true, type: String },
+    categories: { required: true, [categorySchema] }
+});
+
+const GameDetail = mongoose.model("gameDetail", gameDetailSchema);
+
+module.exports = GameDetail;
