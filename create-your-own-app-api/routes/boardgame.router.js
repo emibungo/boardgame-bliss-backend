fb30ce1dfd4cbb6bfea8b1f4dbbb9975fc147d2d
Emi <emi.bungo@gmail.com>
2019-11-06 19:29:33 -0500

    Deleted index.router, moved Delete Single and Get Single methods to gameDetail.router

--- create-your-own-app-api/routes/boardgame.router.js
+++ create-your-own-app-api/routes/boardgame.router.js
@@ -3,14 +3,8 @@ const router = express.Router();
 
 const BoardgameController = require("../controllers/boardgame.controller");
 
-// DELETE Single Game
-router.delete("/:id", BoardgameController.deleteGame);
-
 // GET All Games
 router.get("/", BoardgameController.getAllGames);
-
-// GET Single Game
-router.get("/:id", BoardgameController.getSingleGame);
 
 // POST New Game
 router.post("/", BoardgameController.addNewGame);
