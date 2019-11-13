db4246955864b16894aeb2ebd968c13b812714e6
emibungo <emi.bungo@gmail.com>
2019-11-12 21:36:26 -0500

    Added delete single game and get single game by ID to boardgame router

--- cyoa/routes/boardgame.router.js
+++ cyoa/routes/boardgame.router.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 const BoardgameController = require("../controllers/boardgame.controller");
 
+// DELETE Single Game by ID
+router.delete("/:id", BoardgameController.deleteSingleGame);
+
 // GET All Games
 router.get("/", BoardgameController.getAllGames);
+
+// GET Single Game by ID
+router.get("/:id", BoardgameController.getGameById);
 
 // POST New Game
 router.post("/", BoardgameController.addNewGame);
