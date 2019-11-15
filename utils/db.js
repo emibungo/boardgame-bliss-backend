82ca07c9aa39beab70228b3066ebe13e655f179c
emibungo <emi.bungo@gmail.com>
2019-11-14 22:52:25 -0500

    Added DB link to MongoDB, updated with generic user

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    // "<db-url>",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  );
-  
-  const db = mongoose.connection;
-  
-  db.on("error", console.error.bind(console, "connection error:"));
-  
-  db.once("open", function() {
-    console.log("Connected to the DB!");
-  });
+  "mongodb+srv://gene-rick-user:generic33@cluster0-u970s.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", function() {
+  console.log("Connected to the DB!");
+});
